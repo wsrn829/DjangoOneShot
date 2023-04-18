2756412f8db2214f9b7368e1752dd1a14a9296c9
wsrn829 <wsrn829@gmail.com>
2023-04-18 13:11:40 -0700

    Feature 3 Complete

--- brain_two/todos/models.py
+++ brain_two/todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
