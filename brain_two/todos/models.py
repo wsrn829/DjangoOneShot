417c9b018e69bb661b37a8aa227b6ea9a70f1685
wsrn829 <wsrn829@gmail.com>
2023-04-18 14:41:22 -0700

    Feature 5 Complete

--- brain_two/todos/models.py
+++ brain_two/todos/models.py
@@ -5,3 +5,18 @@ from django.db import models
 class TodoList(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+class TodoItem(models.Model):
+    task = models.CharField(max_length=100)
+    due_date = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    is_completed = models.BooleanField(default=False)
+
+    list = models.ForeignKey(
+        TodoList,
+        related_name="items",
+        on_delete=models.CASCADE,
+    )
