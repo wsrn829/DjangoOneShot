778f16ced08c7e1d051bff8933547b6d2d5342db
wsrn829 <wsrn829@gmail.com>
2023-04-18 13:17:05 -0700

    Feature 4 Complete

--- brain_two/todos/admin.py
+++ brain_two/todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from todos.models import TodoList
+
 
 # Register your models here.
+@admin.register(TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "id",
+    )
