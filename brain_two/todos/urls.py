da3d9b9d49913888746ac00ffb81e2bbe0e4ab1f
wsrn829 <wsrn829@gmail.com>
2023-04-18 18:23:13 -0700

    Feature 8 Complete

--- brain_two/todos/urls.py
+++ brain_two/todos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todos.views import todo_list_list
+from todos.views import todo_list_list, todo_list_detail
 
 urlpatterns = [
     path("", todo_list_list, name="todo_list_list"),
+    path("<int:id>/", todo_list_detail, name="todo_list_detail"),
 ]
--- brain_two/todos/views.py
+++ brain_two/todos/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from todos.models import TodoList
+from django.shortcuts import render, get_object_or_404, redirect
+from todos.models import TodoList, TodoItem
 
 
 # Create your views here.
@@ -9,3 +9,11 @@ def todo_list_list(request):
         "todo_list": todos,
     }
     return render(request, "todos/list.html", context)
+
+
+def todo_list_detail(request, id):
+    todo = get_object_or_404(TodoItem, id=id)
+    context = {
+        "todo_object": todo,
+    }
+    return render(request, "todos/detail.html", context)
